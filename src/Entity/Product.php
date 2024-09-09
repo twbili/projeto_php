8fd16eeb75e97c51eb966e9013fe364930f68e28
ANDERSON MENEZES DO NASCIMENTO <anderson.menezes@demprel.local>
2024-09-09 13:44:37 -0300

    Criado entidade

---
+++ src/Entity/Product.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $Price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $QTD = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(float $Price): static
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getQTD(): ?int
+    {
+        return $this->QTD;
+    }
+
+    public function setQTD(?int $QTD): static
+    {
+        $this->QTD = $QTD;
+
+        return $this;
+    }
+}
